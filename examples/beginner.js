b963502b6a2e67804ca268d31dea2ca73bc50662
benjaminadk <benjaminadk@gmail.com>
2019-06-13 02:01:59 -0700

    :memo: Add examples

---
+++ examples/beginner.js
@@ -0,0 +1,47 @@
+const { createCanvas } = require('canvas')
+const { writeFile } = require('fs')
+const path = require('path')
+const GIFEncoder = require('..')
+
+const size = 400
+const half = 200
+
+const canvas = createCanvas(size, size)
+const ctx = canvas.getContext('2d')
+
+function drawBackground() {
+  ctx.fillStyle = '#ffffff'
+  ctx.fillRect(0, 0, size, size)
+}
+
+const encoder = new GIFEncoder(size, size)
+encoder.setDelay(500)
+encoder.start()
+
+drawBackground()
+ctx.fillStyle = '#ff0000'
+ctx.fillRect(0, 0, half, half)
+encoder.addFrame(ctx)
+
+drawBackground()
+ctx.fillStyle = '#00ff00'
+ctx.fillRect(half, 0, half, half)
+encoder.addFrame(ctx)
+
+drawBackground()
+ctx.fillStyle = '#0000ff'
+ctx.fillRect(half, half, half, half)
+encoder.addFrame(ctx)
+
+drawBackground()
+ctx.fillStyle = '#ffff00'
+ctx.fillRect(0, half, half, half)
+encoder.addFrame(ctx)
+
+encoder.finish()
+
+const buffer = encoder.out.getData()
+
+writeFile(path.join(__dirname, 'output', 'beginner.gif'), buffer, error => {
+  // gif drawn or error
+})
---
+++ examples/intermediate.js
@@ -0,0 +1,56 @@
+const { createCanvas, Image } = require('canvas')
+const { createWriteStream, readdir } = require('fs')
+const { promisify } = require('util')
+const path = require('path')
+const GIFEncoder = require('..')
+
+const readdirAsync = promisify(readdir)
+const imagesFolder = path.join(__dirname, 'input')
+
+async function createGif(algorithm) {
+  return new Promise(async resolve1 => {
+    // read image directory
+    const files = await readdirAsync(imagesFolder)
+
+    // find the width and height of the image
+    const [width, height] = await new Promise(resolve2 => {
+      const image = new Image()
+      image.onload = () => resolve2([image.width, image.height])
+      image.src = path.join(imagesFolder, files[0])
+    })
+
+    // base GIF filepath on which algorithm is being used
+    const dstPath = path.join(__dirname, 'output', `intermediate-${algorithm}.gif`)
+    // create a write stream for GIF data
+    const writeStream = createWriteStream(dstPath)
+    // when stream closes GIF is created so resolve promise
+    writeStream.on('close', () => {
+      resolve1()
+    })
+
+    const encoder = new GIFEncoder(width, height, algorithm)
+    // pipe encoder's read stream to our write stream
+    encoder.createReadStream().pipe(writeStream)
+    encoder.start()
+    encoder.setDelay(200)
+
+    const canvas = createCanvas(width, height)
+    const ctx = canvas.getContext('2d')
+
+    // draw an image for each file and add frame to encoder
+    for (const file of files) {
+      await new Promise(resolve3 => {
+        const image = new Image()
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0)
+          encoder.addFrame(ctx)
+          resolve3()
+        }
+        image.src = path.join(imagesFolder, file)
+      })
+    }
+  })
+}
+
+createGif('neuquant')
+createGif('octree')
